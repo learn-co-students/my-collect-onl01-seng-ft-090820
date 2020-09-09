f8cc01bb4b38d44530a50b8afe217f78ec5e910d
Eli Huebner <elimhuebner@gmail.com>
2020-09-09 23:25:26 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,9 @@
-
-
+def my_collect(collection)
+  i = 0
+  collected_collection = []
+  while i < collection.length
+    collected_collection.push (yield collection[i])
+    i += 1
+  end
+  collected_collection
+end
